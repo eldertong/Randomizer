45cf90fc5190718ffc4328625f7e189cfd3e6fad
JTong <eldertong@gmail.com>
2016-10-06 19:50:53 -0400

    making some crazy changes

--- rand.rb
+++ rand.rb
@@ -1,3 +1,8 @@
 # rand.rb
 
 # ["lyle", "jacob", "jeremy", "ysabele", "lotfi", "cyndi", "don", "stephanie", "susan", "adam", "mike", "maxx", "john"]
+
+def name_array(array)
+    number_of_elements = array.length
+    shuffled = array.shuffled
+    sliced = shuffled.each_slice(2).to_a
