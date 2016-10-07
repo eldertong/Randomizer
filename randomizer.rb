3848c2ac735877ec30f86ce97dfcbc251dbaaaa7
JTong <eldertong@gmail.com>
2016-10-07 12:56:07 -0400

    wrong number of argument

--- randomizer.rb
+++ randomizer.rb
@@ -6,3 +6,10 @@ get '/' do
     erb :get_names
 end
 
+post '/names' do
+    array = params[:user_name1, :user_name2].to_a
+    result = name_array(array)
+    result.to_s
+end
+
+# , :user_name3, :user_name4, :user_name5, :user_name6, :user_name7, :user_name8, :user_name9, :user_nameX, :user_nameX1, :user_nameX2, :user_nameX3
