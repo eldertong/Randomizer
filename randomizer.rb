9f9d5b34d367be7797977aaa85c0440d9e81bd69
JTong <eldertong@gmail.com>
2016-10-07 10:22:27 -0400

    working on app 2

--- randomizer.rb
+++ randomizer.rb
@@ -3,3 +3,9 @@ require 'sinatra'
 
 get '/' do
     erb :get_names
+end
+
+post '/number_of_people_in_class' do
+	number_of_people = params[:number_of_people_in_class]
+	redirect '/names_of_people?number_of_people_in_class=' + number_of_people_in_class
+end
