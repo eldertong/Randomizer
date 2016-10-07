8aa5a2c2f08899618e95ffce20915d330522d935
JTong <eldertong@gmail.com>
2016-10-07 12:36:19 -0400

    still not working

--- randomizer.rb
+++ randomizer.rb
@@ -1,15 +1,8 @@
 require_relative 'rand.rb'
 require 'sinatra'
 
+
 get '/' do
     erb :get_names
 end
 
-post '/get_names' do
-	number_of_people = params[:number_of_people]
-	redirect '/names_of_people?number_of_people=' + number_of_people
-end
-
-get '/names_of_people' do
-    number_of_people = params[:number_of_people]
-end
