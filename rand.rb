099ea3a1f7ab38b44f27587a1b09277318a2ed32
JTong <eldertong@gmail.com>
2016-10-07 08:47:53 -0400

    this is still not pushing the last object in the single array to the other array

--- rand.rb
+++ rand.rb
@@ -4,5 +4,13 @@
 
 def name_array(array)
     number_of_elements = array.length
-    shuffled = array.shuffled
+    shuffled = array.shuffle
     sliced = shuffled.each_slice(2).to_a
+    if shuffled.length % 2 == 1
+        sliced[-2] << sliced[-1][0]
+    else
+        sliced
+    end
+    sliced
+end
+
--- rand_test.rb
+++ rand_test.rb
@@ -2,8 +2,12 @@ require "minitest/autorun"
 require_relative "rand.rb"
 
 class TestIsbn <Minitest::Test
-	def test_empty_array_returns_array
+	def test_array_with_4_names_returns_2
 		array = name_array(["bil", "bob", "sally", "suzy"])
 		assert_equal(2, array.count)
+	end
+	def test_array_with_5_names_returns_3
+		array = name_array(["marv", "bill", "bob", "sally", "suzy"])
+		assert_equal(3, array.count)
 	end
 end
