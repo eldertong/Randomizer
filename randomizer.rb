a65d9faa3f18e1226ade4070d444ab384e44a832
JTong <eldertong@gmail.com>
2016-10-07 10:28:02 -0400

    not working

--- randomizer.rb
+++ randomizer.rb
@@ -5,7 +5,11 @@ get '/' do
     erb :get_names
 end
 
-post '/number_of_people_in_class' do
-	number_of_people = params[:number_of_people_in_class]
-	redirect '/names_of_people?number_of_people_in_class=' + number_of_people_in_class
+post '/get_names' do
+	number_of_people = params[:number_of_people]
+	redirect '/names_of_people?number_of_people=' + number_of_people
+end
+
+get '/names_of_people' do
+    number_of_people = params[:number_of_people]
 end
